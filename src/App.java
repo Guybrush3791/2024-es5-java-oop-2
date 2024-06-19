6f4e0f2fcd171e6ebb0980818cb3ac132da99d3b
Giovanni Cappelletti <tech@methodwolf.com>
2024-06-19 17:04:33 +0200

    update

--- src/App.java
+++ src/App.java
@@ -14,5 +14,51 @@ public class App {
          * creare un array con quei 3 rombi creati e trovare
          * il rombo con l'area maggiore ciclando sull'array
          */
+
+        Rombo r1 = new Rombo(5, 10, 15);
+        Rombo r2 = new Rombo(10, -1, 30);
+        Rombo r3 = new Rombo(0, 30, 45);
+
+        r1.setDiag2(-20);
+
+        System.out.println("--------------------------------");
+        System.out.println(r1);
+        System.out.println("--------------------------------");
+        System.out.println(r2);
+        System.out.println("--------------------------------");
+        System.out.println(r3);
+        System.out.println("--------------------------------");
+
+        Rombo[] rombi = { r1, r2, r3 };
+
+        Rombo maxRombo = null;
+        int maxArea = Integer.MIN_VALUE;
+
+        for (Rombo rombo : rombi) {
+
+            if (rombo.getArea() > maxArea) {
+
+                maxArea = rombo.getArea();
+                maxRombo = rombo;
+            }
+        }
+
+        System.out.println("Il rombo con l'area maggiore è:\n" + maxRombo);
+
+        // Rombo maxRombo = rombi[0];
+        // // int maxArea = Integer.MIN_VALUE;
+        // for (Rombo rombo : rombi) {
+
+        // if (rombo.getArea() > maxRombo.getArea()) {
+
+        // maxRombo = rombo;
+        // }
+        // // if (rombo.getArea() > maxArea) {
+        // // maxArea = rombo.getArea();
+        // // }
+        // }
+
+        // // System.out.println("Il rombo con l'area maggiore è: " + maxArea);
+        // System.out.println("Il rombo con l'area maggiore è:\n" + maxRombo);
     }
 }
--- src/Rombo.java
+++ src/Rombo.java
@@ -24,5 +24,77 @@ public class Rombo {
      * BONUS:
      * - nel caso in cui vengano assegnati valori <= 0 alle
      * variabili, assegnare il valore 1 per evitare errori
+     * 
      */
+
+    int lato;
+    int diag1;
+    int diag2;
+
+    public Rombo(int lato, int diag1, int diag2) {
+
+        setLato(lato);
+        setDiag1(diag1);
+        setDiag2(diag2);
+    }
+
+    public int getLato() {
+
+        return lato;
+    }
+
+    public void setLato(int lato) {
+
+        // if (lato <= 0) {
+        // this.lato = 1;
+        // } else
+        // this.lato = lato;
+
+        this.lato = lato <= 0 ? 1 : lato;
+    }
+
+    public int getDiag1() {
+
+        return diag1;
+    }
+
+    public void setDiag1(int diag1) {
+
+        this.diag1 = diag1 <= 0 ? 1 : diag1;
+    }
+
+    public int getDiag2() {
+
+        return diag2;
+    }
+
+    public void setDiag2(int diag2) {
+
+        this.diag2 = diag2 <= 0 ? 1 : diag2;
+    }
+
+    public int getPerimetro() {
+
+        return 4 * getLato();
+    }
+
+    public int getArea() {
+
+        return (getDiag1() * getDiag2()) / 2;
+    }
+
+    public String getDetails() {
+
+        return "Lato: " + getLato() +
+                "\nDiagonale 1: " + getDiag1() +
+                "\nDiagonale 2: " + getDiag2() +
+                "\nPerimetro: " + getPerimetro() +
+                "\nArea: " + getArea();
+    }
+
+    @Override
+    public String toString() {
+
+        return "Rombo:\n" + getDetails();
+    }
 }
