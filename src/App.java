467cbeca1c8de049378d20ed0dbaafbd8ea07d35
Giovanni Cappelletti <tech@methodwolf.com>
2024-06-19 14:22:45 +0200

    init

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /**
+         * TODO:
+         * 
+         * dopo aver completato la classe Rombo i tutti
+         * i suoi dettagli
+         * 
+         * creare tre oggetto di tipo Rombo con lati diversi
+         * e stampare i dettagli di ciascuno
+         * 
+         * BONUS:
+         * creare un array con quei 3 rombi creati e trovare
+         * il rombo con l'area maggiore ciclando sull'array
+         */
+    }
+}
---
+++ src/Rombo.java
@@ -0,0 +1,28 @@
+public class Rombo {
+
+    /**
+     * TODO:
+     * 
+     * definire variabili private per:
+     * - lato
+     * - diagonale maggiore
+     * - diagonale minore
+     * 
+     * definire costruttore per inizializzare
+     * le variabili tramite le proprieta'
+     * 
+     * definire proprietà getter/setter
+     * 
+     * definire metodo per calcolare
+     * - perimetro: 4 * lato
+     * - area: (diagonale maggiore * diagonale minore) / 2
+     * 
+     * definire metodo per stampare i dettagli
+     * 
+     * definire il metodo toString
+     * 
+     * BONUS:
+     * - nel caso in cui vengano assegnati valori <= 0 alle
+     * variabili, assegnare il valore 1 per evitare errori
+     */
+}
